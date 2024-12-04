a5a9f196523406143486461821e0674591b4e1b9
gio256 <102917377+gio256@users.noreply.github.com>
2024-12-04 16:34:57 -0700

    Rough proof of Homotopic f g iff f = g

--- InfinityCosmos/ForMathlib/AlgebraicTopology/SimplicialSet/Homotopy.lean
+++ InfinityCosmos/ForMathlib/AlgebraicTopology/SimplicialSet/Homotopy.lean
@@ -5,6 +5,7 @@ Authors: Johns Hopkins Category Theory Seminar
 -/
 
 import InfinityCosmos.ForMathlib.AlgebraicTopology.SimplicialSet.CoherentIso
+import Mathlib.AlgebraicTopology.SimplicialSet.StrictSegal
 
 universe u v w
 
@@ -101,6 +102,20 @@ def HomotopicL : Prop :=
 def HomotopicR : Prop :=
     Nonempty (HomotopyR f g)
 
+structure HtpyL (f g : A _[1]) where
+  simplex : A _[2]
+  δ₂ : A.δ 2 simplex = f
+  δ₁ : A.δ 1 simplex = g
+  δ₀ : A.δ 0 simplex = A.σ 0 (A.δ 0 f)
+
+structure HtpyR (f g : A _[1]) where
+  simplex : A _[2]
+  δ₂ : A.δ 2 simplex = A.σ 0 (A.δ 1 f)
+  δ₁ : A.δ 1 simplex = g
+  δ₀ : A.δ 0 simplex = f
+
+def Homotopic (f g : A _[1]) : Prop := Nonempty (HtpyL f g)
+
 def HomotopyR.refl : HomotopyR f f where
   homotopy := standardSimplex.map (σ 0) ≫ f
   face0 := by
@@ -113,6 +128,102 @@ def HomotopyR.refl : HomotopyR f f where
     rw [← Category.assoc, ← Functor.map_comp, ← Category.assoc, ← Functor.map_comp,
         ← δ_comp_σ_of_gt (by simp)]
     rfl
+
+lemma foo {A : SSet.{u}} (s : A _[2]) : (A.spine 2 s).arrow 0 = A.δ 2 s := by
+  have h : mkOfSucc 0 = δ 2 := by
+    ext i
+    fin_cases i <;> rfl
+  simp [SimplicialObject.δ]
+  rw [h]
+
+lemma bar {A : SSet.{u}} (s : A _[2]) : (A.spine 2 s).arrow 1 = A.δ 0 s := by
+  have h : mkOfSucc 1 = δ 0 := by
+    ext i
+    fin_cases i <;> rfl
+  simp [SimplicialObject.δ]
+  rw [h]
+
+lemma baz {A : SSet.{u}} (s : A _[2]) : A.diagonal s = A.δ 1 s := by
+  have h : diag 2 = δ 1 := by
+    ext i
+    fin_cases i <;> rfl
+  simp [SimplicialObject.diagonal, SimplicialObject.δ]
+  rw [h]
+
+open StrictSegal in
+lemma segal_homotopy' [StrictSegal A] (f g : A _[1]) : Homotopic f g ↔ f = g := by
+  have hff : HtpyL f f := sorry
+  apply Iff.intro <;> intro h
+  · have hfg := Classical.choice h
+    have hdg : spineToDiagonal (A.spine 2 hfg.simplex) = g := by
+      simp [spineToDiagonal]
+      rw [spineToSimplex_spine]
+      rw [baz]
+      exact hfg.δ₁
+    have hdf : spineToDiagonal (A.spine 2 hff.simplex) = f := by
+      simp [spineToDiagonal]
+      rw [spineToSimplex_spine]
+      rw [baz]
+      exact hff.δ₁
+    have hs : A.spine 2 hfg.simplex = A.spine 2 hff.simplex := by
+      ext i
+      fin_cases i
+      · simp only [Nat.reduceAdd, Fin.zero_eta, Fin.isValue]
+        simp only [foo]
+        rw [hff.δ₂]
+        exact hfg.δ₂
+      · simp only [Nat.reduceAdd, Fin.mk_one, Fin.isValue]
+        simp only [bar]
+        rw [hff.δ₀]
+        exact hfg.δ₀
+    have hdf : spineToDiagonal (A.spine 2 hfg.simplex) = f := by
+      rw [hs]
+      exact hdf
+    rw [← hdg]
+    exact hdf.symm
+  · apply Nonempty.intro
+    rw [← h]
+    exact hff
+
+/- set_option pp.notation false -/
+open StrictSegal in
+lemma segal_homotopy [StrictSegal A] : HomotopicR f g ↔ f = g := by
+  apply Iff.intro <;> intro h
+  · have h : HomotopyR f g := Classical.choice h
+    apply (SSet.yonedaEquiv _ _).injective
+    apply spineInjective
+    simp [spineEquiv]
+    rw [← Category.id_comp f]
+    rw [← h.face0]
+    /- #check spine A 2 (yonedaEquiv _ _ h.homotopy) -/
+    have hd : diag 2 = δ 1 := by
+      ext i
+      fin_cases i <;> rfl
+    have ha : SimplicialObject.diagonal A (yonedaEquiv _ _ h.homotopy) = yonedaEquiv _ _ g := by
+      simp [SimplicialObject.diagonal]
+      rw [hd]
+      conv => rhs; rw [← h.face1]
+      simp
+      /- rw [← types_comp_apply (A.yonedaEquiv [2]) (A.map _)] -/
+      /- simp [yonedaEquiv, yonedaCompUliftFunctorEquiv] -/
+      /- rw [← types_comp_apply (h.homotopy.app _) (A.map _)] -/
+
+      sorry
+    have hh : spineToDiagonal (spine A 2 (yonedaEquiv _ _ h.homotopy)) = yonedaEquiv _ _ g := by
+      simp [spineToDiagonal]
+      simp [spineToSimplex_spine]
+      simp [SimplicialObject.diagonal]
+
+      sorry
+    ext i
+    fin_cases i
+    simp
+    sorry
+    /- ext n x -/
+    /- apply (SSet.yonedaEquiv _ _).injective -/
+  · apply Nonempty.intro
+    rw [← h]
+    exact HomotopyR.refl f
 
 lemma HomotopyR.equiv : --[Quasicategory A] :
     Equivalence (fun f g : Δ[1] ⟶ A ↦ HomotopicR f g) where
