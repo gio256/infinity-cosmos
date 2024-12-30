d41260967cd167b105eda5310ea000958ec07d4b
gio256 <102917377+gio256@users.noreply.github.com>
2024-12-30 14:39:04 -0700

    Rough proof of boundary as coequalizer

--- InfinityCosmos/ForMathlib/AlgebraicTopology/SimplicialSet/Delta.lean
+++ InfinityCosmos/ForMathlib/AlgebraicTopology/SimplicialSet/Delta.lean
@@ -15,24 +15,14 @@ lemma δ_not_surjective {n : ℕ} (i : Fin (n + 2)) :
     ¬Function.Surjective (δ i).toOrderHom :=
   not_surjective_of_card_lt _ (by simp)
 
-/-- Any `j` not in the image of `δ i` must be equal to `i`. -/
-lemma δ_skips_eq {n : ℕ} (i j : Fin (n + 2)) (h : ¬∃ a, (δ i).toOrderHom a = j) :
-    j = i := by simp_all [δ]
-
 noncomputable def skipped {n : ℕ} {a : SimplexCategoryᵒᵖ} (α : ∂Δ[n].obj a) :
     Fin (n + 1) := Classical.choose <| not_forall.mp α.property
 
-def beta {n : ℕ} {m : SimplexCategoryᵒᵖ} (α : Δ[n + 1].obj m)
-    (i : Fin (n + 2)) : Δ[n].obj m :=
-  standardSimplex.map (σ (Fin.predAbove 0 i)) |>.app m α
-  /- objEquiv [n] m |>.symm <| factor_δ (m := m.unop.len) (objEquiv [n + 1] m α) i -/
-
 section PredAbove
 open Fin
-
 variable {n : ℕ}
 
-lemma predAbove_eq_lt_iff {p j : Fin n} (i : Fin (n + 1)) (h : j < p) :
+lemma predAbove_eq_lt_iff {p j : Fin n} (h : j < p) (i : Fin (n + 1)) :
     p.predAbove i = j ↔ i = j.castSucc := by
   by_cases hi : p.castSucc < i
   · apply Iff.intro <;> rintro rfl
@@ -55,7 +45,7 @@ lemma predAbove_eq_iff (p : Fin n) (i : Fin (n + 1)) :
     · rw [predAbove_castSucc_self]
     · rw [predAbove_succ_self]
 
-lemma predAbove_eq_gt_iff {p j : Fin n} (i : Fin (n + 1)) (h : p < j) :
+lemma predAbove_eq_gt_iff {p j : Fin n} (h : p < j) (i : Fin (n + 1)) :
     p.predAbove i = j ↔ i = j.succ := by
   by_cases hi : p.castSucc < i
   . rw [predAbove_of_castSucc_lt _ _ hi]
@@ -65,19 +55,139 @@ lemma predAbove_eq_gt_iff {p j : Fin n} (i : Fin (n + 1)) (h : p < j) :
       exact False.elim <| hi <| (lt_castPred_iff _).mp h
     · exact False.elim <| lt_asymm h <| succ_le_castSucc_iff.mp <| not_lt.mp hi
 
+lemma predAbove_lt_iff_lt_castSucc (p : Fin n) (i : Fin (n + 1)) :
+    p.predAbove i < p ↔ i < p.castSucc := by
+  apply Iff.intro
+  · intro h
+    by_cases hi : p.castSucc < i
+    · rw [predAbove_of_castSucc_lt _ _ hi] at h
+      exact False.elim <| not_lt.mpr ((le_pred_iff _).mpr hi) h
+    · rw [predAbove_of_le_castSucc _ _ (not_lt.mp hi)] at h
+      exact h
+  · intro h
+    rw [predAbove_of_le_castSucc _ _ (le_of_lt h)]
+    exact h
+
+lemma lt_predAbove_iff_succ_lt (p : Fin n) (i : Fin (n + 1)) :
+    p < p.predAbove i ↔ p.succ < i := by
+  apply Iff.intro
+  · intro h
+    by_cases hi : p.castSucc < i
+    · rw [predAbove_of_castSucc_lt _ _ hi] at h
+      exact lt_pred_iff (ne_zero_of_lt hi) |>.mp h
+    · rw [predAbove_of_le_castSucc _ _ (not_lt.mp hi)] at h
+      exact False.elim <| hi h
+  · intro h
+    rw [predAbove_of_succ_le _ _ (le_of_lt h)]
+    exact lt_pred_iff (ne_zero_of_lt h) |>.mpr h
+
 end PredAbove
 
-lemma σ_eq_lt_iff {n : ℕ} (i k : Fin (n + 1)) (h :  k < i)
-    (j : Fin (n + 2)) : (σ i).toOrderHom j = k ↔ j = k.castSucc :=
-  predAbove_eq_lt_iff j h
+section SuccAbove
+open Fin
+variable {n : ℕ} {p : Fin (n + 1)}
 
-lemma σ_eq_iff {n : ℕ} (i : Fin (n + 1)) (j : Fin (n + 2)) :
-    (σ i).toOrderHom j = i ↔ j = i.castSucc ∨ j = i.succ :=
-  predAbove_eq_iff i j
+lemma succAbove_eq_lt_iff {j : Fin (n + 1)} (h : j < p) (i : Fin n) :
+    p.succAbove i = j ↔ i.castSucc = j := by
+  by_cases hi : i.castSucc < p
+  · rw [succAbove_of_castSucc_lt _ _ hi]
+  · apply Iff.intro <;> rintro rfl
+    · rw [succAbove_of_le_castSucc _ _ (not_lt.mp hi)] at h
+      exact False.elim <| hi <| lt_succ |>.trans h
+    · exact False.elim <| hi h
 
-lemma σ_eq_gt_iff {n : ℕ} (i k : Fin (n + 1)) (h : i < k) (j : Fin (n + 2)) :
-    (σ i).toOrderHom j = k ↔ j = k.succ :=
-  predAbove_eq_gt_iff j h
+lemma succAbove_eq_gt_iff {j : Fin (n + 1)} (h : p < j) (i : Fin n) :
+    p.succAbove i = j ↔ i.succ = j := by
+  by_cases hi : i.castSucc < p
+  · apply Iff.intro <;> rintro rfl
+    · rw [succAbove_of_castSucc_lt _ _ hi] at h
+      exact False.elim <| lt_asymm hi h
+    · exact False.elim <| (not_lt.mpr (castSucc_lt_iff_succ_le.mp hi)) h
+  · rw [succAbove_of_le_castSucc _ _ (not_lt.mp hi)]
+
+end SuccAbove
+
+def beta {n : ℕ} {m : SimplexCategoryᵒᵖ} (α : Δ[n + 1].obj m)
+    (i : Fin (n + 2)) : Δ[n].obj m :=
+  standardSimplex.map (σ (Fin.predAbove 0 i)) |>.app m α
+
+def gamma {n : ℕ} {m : SimplexCategoryᵒᵖ} (α : Δ[n + 2].obj m)
+    (i : Fin (n + 2)) (j : Fin (n + 3)) : Δ[n].obj m := beta (beta α j) i
+
+open Fin in
+lemma δ_gamma {n : ℕ} {m : SimplexCategoryᵒᵖ} (α : Δ[n + 2].obj m)
+    {i : Fin (n + 2)} (hi : ∀ x, (asOrderHom α) x ≠ i.castSucc)
+    {j : Fin (n + 3)} (hj : ∀ x, (asOrderHom α) x ≠ j) (h : i.castSucc < j) :
+    (standardSimplex.map (δ i)).app m (gamma α i j) = beta α j := by
+  simp [gamma, beta, standardSimplex, uliftFunctor]
+  rw [predAbove_zero_of_ne_zero (ne_zero_of_lt h)]
+  change factor_δ (m := m.unop.len) (_ ≫ σ _) i ≫ δ i = _
+  apply factor_δ_spec
+  intro k
+  dsimp [σ, δ]
+  obtain hlt | heq := lt_or_eq_of_le <| le_pred_iff _ |>.mpr h
+  · rw [predAbove_eq_lt_iff hlt]
+    exact hi k
+  · rw [← heq, predAbove_eq_iff]
+    rintro (hn | hn)
+    · exact hi k hn
+    · simp only [heq, succ_pred] at hn
+      exact hj k hn
+
+open Fin in
+@[reassoc]
+lemma σ_predAbove_comp_σ_predAbove_lt {n : ℕ} {i : Fin (n + 2)} {j : Fin (n + 3)} (h : i.castSucc < j) :
+    σ (predAbove 0 j) ≫ σ (predAbove 0 i) =
+      σ (predAbove 0 i.castSucc) ≫ σ (predAbove 0 (j.pred (ne_zero_of_lt h))) := by
+  rw [predAbove_zero_of_ne_zero (ne_zero_of_lt h), ← castSucc_zero,
+    castSucc_predAbove_castSucc, σ_comp_σ]
+  · obtain hlt | heq := lt_or_eq_of_le <| le_pred_iff (ne_zero_of_lt h) |>.mpr h
+    · rw [predAbove_zero_of_ne_zero (ne_zero_of_lt hlt), succ_pred]
+    · rw [← heq]
+      cases' i using cases with i
+      · rw [← σ_comp_σ (by rfl), predAbove_right_zero, castSucc_zero]
+      · rw [predAbove_zero_succ]
+  · cases' i using cases with i
+    · simp only [predAbove_right_zero, zero_le]
+    · rw [← succ_castSucc] at h
+      have h := lt_pred_iff (ne_zero_of_lt h) |>.mpr h
+      rw [predAbove_zero_succ, predAbove_zero_of_ne_zero (ne_zero_of_lt h)]
+      exact le_pred_iff _ |>.mpr h
+
+open Fin in
+lemma δ_gamma' {n : ℕ} {m : SimplexCategoryᵒᵖ} (α : Δ[n + 2].obj m)
+    {i : Fin (n + 2)} (hi : ∀ x, (asOrderHom α) x ≠ i.castSucc)
+    {j : Fin (n + 3)} (hj : ∀ x, (asOrderHom α) x ≠ j) (h : i.castSucc < j) :
+    (standardSimplex.map (δ (j.pred (ne_zero_of_lt h)))).app m (gamma α i j) =
+      beta α i.castSucc := by
+  simp [gamma, beta, standardSimplex, uliftFunctor]
+  rw [σ_predAbove_comp_σ_predAbove_lt_assoc h]
+  change factor_δ (m := m.unop.len) (_ ≫ σ _) (j.pred _) ≫ δ _ = _
+  rw [factor_δ_spec]
+  intro k
+  dsimp [σ]
+  obtain hlt | heq := lt_or_eq_of_le <| le_pred_iff (ne_zero_of_lt h) |>.mpr h
+  · cases' i using cases with i
+    · simp only [castSucc_zero, predAbove_right_zero]
+      erw [predAbove_zero_of_ne_zero (hi k)]
+      rw [pred_inj]
+      exact hj k
+    · nth_rewrite 1 [← succ_castSucc i]
+      rw [predAbove_zero_succ]
+      intro hn
+      have := predAbove_eq_gt_iff (lt_succ.trans hlt) _ |>.mp hn
+      simp at this
+      exact hj k this
+  · rw [← heq]
+    cases' i using cases with i
+    · simp only [castSucc_zero, predAbove_right_zero]
+      erw [predAbove_zero_of_ne_zero (hi k)]
+      aesop
+    · nth_rewrite 1 [← succ_castSucc]
+      erw [predAbove_zero_succ]
+      rw [predAbove_eq_gt_iff (castSucc_lt_succ i)]
+      rw [heq, succ_pred]
+      exact hj k
 
 def diagram (n : ℕ) : MultispanIndex SSet where
   L := { (i, j) : Fin (n + 2) × Fin (n + 3) | i.castSucc < j }
@@ -111,11 +221,12 @@ abbrev cocone (n : ℕ) : Multicofork (diagram n) := by
     rw [← δ_comp_δ' h]
     rfl
 
+set_option pp.coercions false
 open Fin standardSimplex Opposite in
 lemma beta_pi {n : ℕ} (X : Multicofork (diagram n))
     {m : SimplexCategoryᵒᵖ} (α : Δ[n + 2].obj m)
-    (i : Fin (n + 3))  (hi : ∀ x, (asOrderHom α) x ≠ i)
-    (j : Fin (n + 3))  (hj : ∀ x, (asOrderHom α) x ≠ j) :
+    (i : Fin (n + 3)) (hi : ∀ x, (asOrderHom α) x ≠ i)
+    (j : Fin (n + 3)) (hj : ∀ x, (asOrderHom α) x ≠ j) :
     (X.π i).app m (beta α i) = (X.π j).app m (beta α j) := by
   obtain rfl | hij | hji : i = j ∨ i < j ∨ j < i := by omega
   · rfl
@@ -123,49 +234,24 @@ lemma beta_pi {n : ℕ} (X : Multicofork (diagram n))
     have hi' : i = i'.castSucc := by aesop
     let ij : { (i, j) : Fin (n + 2) × Fin (n + 3) | i.castSucc < j } :=
       ⟨(i', j), hij⟩
-    let γ := beta (beta α j) i'
-    have hu : ((diagram n).snd ij).app m γ = beta α j := by
-      simp [diagram, γ, beta]
-      simp [standardSimplex, uliftFunctor]
-      rw [Fin.predAbove_zero_of_ne_zero (Fin.ne_zero_of_lt hij)]
-      change factor_δ (m := m.unop.len) (_ ≫ σ _) i' ≫ δ i' = _
-      rw [factor_δ_spec]
-      intro k
-      simp only [comp_toOrderHom, OrderHom.comp_coe, Function.comp_apply]
-      let jp := j.pred (Fin.ne_zero_of_lt hij)
-      have hh : i' ≤ jp :=
-        (castPred_le_pred_iff (ne_last_of_lt hij) (Fin.ne_zero_of_lt hij)).mpr hij
-      obtain hh | hh : i' < jp ∨ i' = jp := Fin.lt_or_eq_of_le hh
-      · simp
-        have := σ_eq_lt_iff jp i' hh (asOrderHom α k)
-        simp [asOrderHom] at this
-        rw [this]
-        exact hi k
-      · simp
-        simp [jp] at hh
-        rw [← hh]
-        have := σ_eq_iff i' (asOrderHom α k)
-        simp [asOrderHom] at this
-        rw [this]
-        intro hn
-        rcases hn with hn | hn
-        · exact hi k hn
-        · rw [hh] at hn
-          simp at hn
-          exact hj k hn
-
-    have hv : ((diagram n).fst ij).app m γ = beta α i := by
-      simp [diagram, γ, beta]
-      simp [standardSimplex, uliftFunctor]
-      rw [Fin.predAbove_zero_of_ne_zero (Fin.ne_zero_of_lt hij)]
-      sorry
-
+    let γ := gamma α i' j
+    rw [hi'] at hi
+    have hu : ((diagram n).snd ij).app m γ = beta α j := δ_gamma α hi hj hij
+    have hv : ((diagram n).fst ij).app m γ = beta α i := δ_gamma' α hi hj hij
     rw [← hu, ← hv]
     rw [← types_comp_apply _ ((X.π _).app m), ← types_comp_apply _ ((X.π _).app m)]
     rw [← NatTrans.comp_app, ← NatTrans.comp_app]
     erw [← X.condition ij]
     rfl
-  · sorry
+  · let j' := j.castPred <| ne_last_of_lt hji
+    let ji : { (j, i) : Fin (n + 2) × Fin (n + 3) | j.castSucc < i } :=
+      ⟨(j', i), hji⟩
+    rw [← castSucc_castPred j (ne_last_of_lt hji)] at hj ⊢
+    rw [← δ_gamma α hj hi hji, ← δ_gamma' α hj hi hji]
+    rw [← types_comp_apply _ ((X.π _).app m), ← types_comp_apply _ ((X.π _).app m)]
+    rw [← NatTrans.comp_app, ← NatTrans.comp_app]
+    erw [← X.condition ji]
+    rfl
 
 /- set_option pp.proofs true -/
 open Opposite standardSimplex in
